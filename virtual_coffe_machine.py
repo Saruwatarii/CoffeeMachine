942ba0a7bbaaf12d0477f94244b97a98a1108c7d
Tim Aadland <potetstapp@gmail.com>
2020-08-10 07:59:08 +0200

    Added a funtion that calculate the amount of milk, water and coffee beans needed to make from the number of cups the user wanna make. Then print out a string with with the amount of each ingredients.

---
+++ virtual_coffe_machine.py
@@ -0,0 +1,21 @@
+def coffee_maker(cups):
+    '''
+    (int) --> str(int, int, int)
+
+    Cups gets calculated with multiplication with water, milk and coffee_beans.
+    Cups can be adjusted by how much it is necessary to make the specified amount of coffee
+    :param cups:
+    :return: return a string with the variables multiplied with the parameter
+    '''
+    water = 200 * cups
+    milk = 50 * cups
+    coffee_beans = 15 * cups
+
+    print(f"{water} ml of water\n{milk} ml of milk\n{coffee_beans} g of coffee beans")
+
+# prompt the user
+coffee_user = int(input("Write how many cups of coffee you will need: "))
+# Print out the prompt and a string
+print(f"For {coffee_user} of coffee you will need: ")
+# Call the function coffee_maker with the argument coffee_user
+coffee_maker(coffee_user)
