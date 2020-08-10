79b1c7f9227585267e7d980c8112d8d8aaf9e48e
Tim Aadland <potetstapp@gmail.com>
2020-08-10 20:21:40 +0200

    Made it wokr like aits sumulating a coggee machine. Where it starts off with limited supply of the different ingredients and disposable cups, and also calculating how much coffee is costing. Added three types such as: Espresso, Latte and Cappuccino. Added a starting menu with (buy, fill and take). When choosing buy, you can choose between the three types of coffee. Fill if you want to fill up the machine with more supplies. And lastly take, which takes all the money in the coffee machine.

--- virtual_coffe_machine.py
+++ virtual_coffe_machine.py
@@ -27,15 +27,83 @@ print(f"For {coffee_user} of coffee you will need: ")
 coffee_maker(coffee_user)
 '''
 #-------------------------------------------------------------------------------------------------------
+
+
+def coffe_overview(water, milk, bean, cup, money):
+    print(f"The coffee machine has:\n"
+          f"{water_machine} of water\n"
+          f"{milk_machine} of milk\n"
+          f"{bean_machine} of coffee beans\n"
+          f"{cups_machine} of disposable cups\n"
+          f"{money_machine} of money\n")
+
+'''
 water = 200
 milk = 50
 coffee_beans = 15
+'''
+water_machine = 400
+milk_machine = 540
+bean_machine = 120
+cups_machine = 9
+money_machine = 550
 
-water_container = int(input("Write how many ml of water the coffee machine has: "))
-milk_container = int(input("Write how many ml of milk the coffee machine has: "))
-coffee_beans_container = int(input("Write how many grams of coffee beans the coffee machine has: "))
+espresso = 4
+latte = 7
+cappuccino = 6
+
+coffe_overview(water_machine, milk_machine, bean_machine, cups_machine, money_machine)
+starter_menu = input("Write action (buy, fill, take): ").lower()
+
+if starter_menu == "buy":
+    type_coffee = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:"))
+    if type_coffee == 1:
+        money_machine += espresso
+        water_machine = water_machine - 250
+        bean_machine = bean_machine - 16
+        cups_machine = cups_machine - 1
+        print()
+        coffe_overview(water_machine, milk_machine, bean_machine, cups_machine, money_machine)
+    elif type_coffee == 2:
+        money_machine += latte
+        water_machine = water_machine - 350
+        milk_machine = milk_machine - 75
+        bean_machine = bean_machine - 12
+        cups_machine = cups_machine - 1
+        print()
+        coffe_overview(water_machine, milk_machine, bean_machine, cups_machine, money_machine)
+    else:
+        money_machine += cappuccino
+        water_machine = water_machine - 200
+        milk_machine = 100
+        bean_machine = 120
+        cups_machine = cups_machine - 1
+        print()
+        coffe_overview(water_machine, milk_machine, bean_machine, cups_machine, money_machine)
+
+
+elif starter_menu == "fill":
+    water_container = int(input("Write how many ml of water do you want to add: "))
+    milk_container = int(input("Write how many ml of milk do you want to add: "))
+    coffee_beans_container = int(input("Write how many grams of coffee beans do you want to add: "))
+    disposable_cups = int(input("Write how many disposable cups of coffee do you want to add: "))
+
+    water_machine += water_container
+    milk_machine += milk_container
+    bean_machine += coffee_beans_container
+    cups_machine += disposable_cups
+
+    coffe_overview(water_machine, milk_machine, bean_machine, cups_machine, money_machine)
+
+elif starter_menu == "take":
+    print(f"I gave you ${money_machine}\n")
+    money_machine -= money_machine
+    coffe_overview(water_machine, milk_machine, bean_machine, cups_machine, money_machine)
+
+
+
+'''
 many_cups_coffee = int(input("Write how many cups of coffee you will need: "))
-
 # How many cups of each ingredient its in the container
 water_cup =  water_container // water
 milk_cup = milk_container // milk
@@ -49,7 +117,7 @@ elif possible_coffee_cups == many_cups_coffee:
     print("Yes, I can make that amount of coffee")
 else:
     print(f"Yes, I can make that amount of coffee (and even {possible_coffee_cups - many_cups_coffee} more than that)")
-
+'''
 
 
 
