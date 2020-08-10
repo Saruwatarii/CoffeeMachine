f0f7943f9f15ca5477355bb48b73dc80a892de9b
Tim Aadland <potetstapp@gmail.com>
2020-08-10 16:43:46 +0200

    Added reguests the amounts of water, milk and coffee beans thats available at the moment. If yes, prints out that it have, and if it have more ingredients to make more cups of coffee, it will print out the yes it can make, and also how many cups, the same if the user request too many regarding the resources to make a cup of coffee.

--- virtual_coffe_machine.py
+++ virtual_coffe_machine.py
@@ -1,5 +1,6 @@
+'''
 def coffee_maker(cups):
-    '''
+
     (int) --> str(int, int, int)
 
     Cups gets calculated with multiplication with water, milk and coffee_beans.
@@ -12,16 +13,47 @@ def coffee_maker(cups):
         print(f"{24000} ml of water
         {6250} ml of milk
         {1875} g of coffee beans")
-    '''
+
     water = 200 * cups
     milk = 50 * cups
     coffee_beans = 15 * cups
 
     print(f"{water} ml of water\n{milk} ml of milk\n{coffee_beans} g of coffee beans")
 
-# prompt the user
 coffee_user = int(input("Write how many cups of coffee you will need: "))
-# Print out the prompt and a string
+
 print(f"For {coffee_user} of coffee you will need: ")
-# Call the function coffee_maker with the argument coffee_user
+
 coffee_maker(coffee_user)
+'''
+#-------------------------------------------------------------------------------------------------------
+water = 200
+milk = 50
+coffee_beans = 15
+
+water_container = int(input("Write how many ml of water the coffee machine has: "))
+milk_container = int(input("Write how many ml of milk the coffee machine has: "))
+coffee_beans_container = int(input("Write how many grams of coffee beans the coffee machine has: "))
+many_cups_coffee = int(input("Write how many cups of coffee you will need: "))
+
+# How many cups of each ingredient its in the container
+water_cup =  water_container // water
+milk_cup = milk_container // milk
+coffe_bean_cup = coffee_beans_container // coffee_beans
+# Find the total cup of coffee from ingredients
+possible_coffee_cups = min(water_cup, milk_cup, coffe_bean_cup)
+
+if possible_coffee_cups < many_cups_coffee:
+    print(f"No, I can make only {possible_coffee_cups} cups of coffee")
+elif possible_coffee_cups == many_cups_coffee:
+    print("Yes, I can make that amount of coffee")
+else:
+    print(f"Yes, I can make that amount of coffee (and even {possible_coffee_cups - many_cups_coffee} more than that)")
+
+
+
+
+
+
+
+
